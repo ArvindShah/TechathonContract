316cfb74bf409e9335679951a5e5bb88b2ebe5e4
ArvindkrShah <57900018+ArvindkrShah@users.noreply.github.com>
2020-11-01 04:14:42 +0530

    changed method

--- TechathonContract/ClientApp/src/app/home/home.component.ts
+++ TechathonContract/ClientApp/src/app/home/home.component.ts
@@ -23,34 +23,11 @@ export class HomeComponent implements OnInit{
     let fileToUpload = <File>files[0];
     const formData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
-    //this.http.post('https://localhost:44320/WeatherForecast/Upload', formData, { reportProgress: true, observe: 'events' })
-    //  .subscribe(event => {
-    //    if (event.type === HttpEventType.UploadProgress)
-    //      this.progress = Math.round(100 * event.loaded / event.total);
-    //    else if (event.type === HttpEventType.Response) {
-    //      this.message = 'Upload success.';
-    //    }
-    //  });
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    //let options = new RequestOptions({ headers: headers });
-
-    this.http.get<any>(this.baseUrl + 'contract/uploadget').subscribe(event => {
-      (r) => { console.log('got r', r) }
-    }, error => console.error(error));
+    
 
     this.http.post<any>(this.baseUrl + 'contract/UploadFiles', formData).subscribe(event => {
       (r) => { console.log('got r', r) }
     }, error => console.error(error));
 
-    const uploadReq = new HttpRequest('POST', `WeatherForecast/Upload`, formData, {
-      reportProgress: true,
-    });
-
-    this.http.request(uploadReq).subscribe(event => {
-      if (event.type === HttpEventType.UploadProgress)
-        this.progress = Math.round(100 * event.loaded / event.total);
-      else if (event.type === HttpEventType.Response)
-        this.message = event.body.toString();
-    });
   }
 }
