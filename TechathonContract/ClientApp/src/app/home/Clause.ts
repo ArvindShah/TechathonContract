825915ad7d2bdfcacab51084efd658718ffb3111
Ronak Karani <Ronak.Karani@gep.com>
2020-11-02 02:37:39 +0530

    Home changes

---
+++ TechathonContract/ClientApp/src/app/home/Clause.ts
@@ -0,0 +1,10 @@
+export class Clause
+    {
+        ContentId : number;
+        ContentName : string;
+        ContentDescription : string
+        isClause : boolean;
+        IsEditable : boolean;
+        Comment : string
+
+    }
