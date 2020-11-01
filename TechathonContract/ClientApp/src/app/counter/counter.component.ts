e9542a96a6b86e3416ab834f606e0b88042d83d9
Ronak Karani <Ronak.Karani@gep.com>
2020-11-01 14:59:22 +0530

    Admin Page HTML changes

--- TechathonContract/ClientApp/src/app/counter/counter.component.ts
+++ TechathonContract/ClientApp/src/app/counter/counter.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Contract } from './Contract';
+import { User } from './User';
 
 @Component({
   selector: 'app-counter-component',
@@ -6,8 +8,18 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
   public currentCount = 0;
+  public document;
+  public user;
+  public access;
+  public documentList:Contract;
+  public userList:User;
+  public 
 
   public incrementCounter() {
     this.currentCount++;
+  }
+
+  public updateControl(){
+    console.log( this.document );
   }
 }
